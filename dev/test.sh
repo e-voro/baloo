98c27b94fb93ed40928c6166dd241793ba3b1614
Evgeny Voropaev <evorop@gmail.com>
2026-03-05 21:46:39 +0800

    In tets.sh new tactic is a full testing: 'make check' plus make check-world'. Also add new examples in test.sh.

--- dev/test.sh
+++ dev/test.sh
@@ -8,7 +8,8 @@ DBG_PROJ_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.."; pwd)"
 
 make check
 #make check-world
-#make -C "$DBG_PROJ_DIR" check-world PG_TEST_EXTRA="wal_consistency_checking xid_wraparound "
+make check-world PG_TEST_EXTRA="wal_consistency_checking"
+#make -C "$DBG_PROJ_DIR" check-world PG_TEST_EXTRA="wal_consistency_checking xid_wraparound"
 #make -C "$DBG_PROJ_DIR" check-world \
 #          PG_TEST_EXTRA=" ssl \
 #                          ldap \
@@ -24,5 +25,6 @@ make check
 #make -C "$DBG_PROJ_DIR/src/bin/pg_amcheck" check
 #make -C "$DBG_PROJ_DIR/src/bin/pg_amcheck" check PROVE_TESTS="t/004_verify_heapam.pl"
 #make -C "$DBG_PROJ_DIR/src/bin/pg_amcheck" check PROVE_TESTS="t/004_verify_heapam.pl" PROVE_FLAGS=" --verbose --exec 'perl -d -I ${DBG_PROJ_DIR}/src/test/perl/ -I . -Ilib -Ilocal/lib/perl5'"
-#make -C "$DBG_PROJ_DIR/src/test/recovery/" check PROVE_TESTS="t/027_stream_regress.pl" PG_TEST_EXTRA="wal_consistency_checking"
+# make -C "$DBG_PROJ_DIR/src/test/recovery/" check PROVE_TESTS="t/027_stream_regress.pl" PG_TEST_EXTRA="wal_consistency_checking"
 #make -C "$DBG_PROJ_DIR/src/test/recovery/" check PROVE_TESTS="t/043_single_tuple_page_logical_backup.pl" PROVE_FLAGS=" --verbose --exec 'perl -d -I ${DBG_PROJ_DIR}/src/test/perl/ -I . -Ilib -Ilocal/lib/perl5'"
+# make -C "$DBG_PROJ_DIR/src/test/recovery/" check PROVE_TESTS="t/016_min_consistency.pl " PROVE_FLAGS=" --verbose"
