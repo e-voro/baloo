b321ccb6531532baf101a3cd5c3f1f371b69a807
Evgeny Voropaev <evorop@gmail.com>
2026-08-05 18:48:37 +0800

    Turn off shared preload libraries by default.

--- dev/devcfgli.sh
+++ dev/devcfgli.sh
@@ -374,10 +374,10 @@ inst_init() {
 	echo " io_method = worker # worker, io_uring, sync (change requires restart) " >> \$PGDATA/postgresql.conf
 	echo " io_workers = 1 " >> \$PGDATA/postgresql.conf
 
-    echo "---------------------------------------------------------"
-    echo "Configuring SHARED PRELOAD LIBRARIES"
-    echo "---------------------------------------------------------"
-    echo "shared_preload_libraries='online_analyze,plantuner,dbcopies_decoding,test_decoding,test_aio'" >> \$PGDATA/postgresql.conf
+    #echo "---------------------------------------------------------"
+    #echo "Configuring SHARED PRELOAD LIBRARIES"
+    #echo "---------------------------------------------------------"
+    #echo "shared_preload_libraries='online_analyze,plantuner,dbcopies_decoding,test_decoding,test_aio'" >> \$PGDATA/postgresql.conf
 
     echo "========================================================="
     echo "BALOO: CLUSTER INITIALISATION FINISHED"
